32d630237d6671b4239852ae4a21f947203b0a10
stargazer <stargazer@stargazer.local>
2020-07-22 22:46:43 +0530

    login chq1

--- lib/views/chatroom_screen.dart
+++ lib/views/chatroom_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_chat/services/auth.dart';
+import 'package:flutter_chat/views/signin.dart';
 
 class ChatRoom extends StatefulWidget {
   @override
@@ -6,10 +8,28 @@ class ChatRoom extends StatefulWidget {
 }
 
 class _ChatRoomState extends State<ChatRoom> {
+  AuthMethods authMethods = new AuthMethods();
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/logo.png",
+          height: 50.0,
+        ),
+        actions: <Widget>[
+          GestureDetector(    
+            onTap: () {
+              authMethods.signOut();      
+              Navigator.pushReplacement(    
+                  context, MaterialPageRoute(builder: (context)=>SignIn()));
+            },
+            child: Container(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Icon(Icons.exit_to_app)),
+          ),
+        ],
+      ),
     );
   }
 }
